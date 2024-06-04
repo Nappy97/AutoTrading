5c3bd7b8349c1c3898d019304a64c67afcdcb81d
정의형 <wjddmlgud829@gmail.com>
2024-06-04 22:53:37 +0900

    임시

---
+++ AutoTrading.Application/Common/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using AutoTrading.Application.Users.Login;
+using AutoTrading.Application.Users.Register;
+
+namespace AutoTrading.Application.Common.Interfaces;
+
+public interface IUserService
+{
+    Task<RegistrationResponse> RegisterUserAsync(RegisterUserDTO registerUserDto);
+
+    Task<LoginResponse> LoginUserAsync(LoginDTO loginDto);
+}
